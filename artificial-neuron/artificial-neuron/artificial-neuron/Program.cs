0715782b1cbcc18f5856482ff0e87e062cdc1402
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-10-11 22:07:48 +0300

    d

--- artificial-neuron/artificial-neuron/artificial-neuron/Program.cs
+++ artificial-neuron/artificial-neuron/artificial-neuron/Program.cs
@@ -4,9 +4,36 @@ namespace artificial_neuron
 {
     class Program
     {
+        class Neuron
+        {
+            private decimal weight = 0.5m;
+            public decimal LastError { get; private set; }
+            public decimal Smothing { get; set; } = 0.00001m;
+            public decimal ProcessInputData(decimal input)
+            {
+                return input * weight;
+            }
+            public decimal RestoreInputData(decimal output)
+            {
+                return output / weight;
+            }
+            public void Train(decimal input, decimal expectedResult)
+            {
+                var actualResult = input * weight;
+                LastError = expectedResult - actualResult;
+                var correction = (LastError / actualResult) * Smothing;
+                weight += correction;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal km = 100;
+            decimal miles = 62.1371m;
+
+            Neuron neuron = new Neuron();
+          
+            Console.WriteLine(neuron.ProcessInputData(km));
         }
     }
 }
