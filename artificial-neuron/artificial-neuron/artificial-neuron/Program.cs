9af0b15c8ffad4d615b57e6dbf015b0f72119fb9
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-10-11 21:18:11 +0300

    cv

---
+++ artificial-neuron/artificial-neuron/artificial-neuron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace artificial_neuron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
